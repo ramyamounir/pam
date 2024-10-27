b442fbd99152a540fec9b1f24c4772309b8d3ed6
Ramy Mounir <ramyamounir@gmail.com>
2024-10-27 07:20:11 -0400

    assets for readme

--- README.md
+++ README.md
@@ -1,31 +1,62 @@
-# solo-sdr
-HTM prediction with associative memory
+# Predictive Attractor Models
+Official repository for the paper "[Predictive Attractor Models](https://arxiv.org/abs/2410.02430)" published at NeurIPS 2024.
 
 <p align="center">
-  <img src="assets/full.png" alt="Full architecture of this project"/>
+  <img src="assets/ssm.png" alt="Full architecture of this project"/>
 </p>
 
 ---
 
 ## Environment Setup
-1. Create a conda environment with `conda create -n pamenv python=3.11 -y` and activate it with `conda activate pamenv`
+1. Clone repository
+```sh
+git clone git@github.com:ramyamounir/pam.git
+```
+
+
+2. Create and activate conda environment 
+```sh
+conda create -n pamenv python=3.11 -y 
+conda activate pamenv
+
+# Install pytorch
+conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
+```
+
+3. Use PamModel to learn and generate sequences
+```python
+from src.models.pam import PamModel
+from src.utils.configs import get_pam_configs
+from src.utils.exps import accuracy_SDR
+from src.utils.sdr import SDR
+
+# create two sequence
+seq1 = [SDR(N=100, S=5) for _ in range(10)]
+seq2 = [SDR(N=100, S=5) for _ in range(10)]
+
+# Instantiate model with default parameters
+pam = PamModel(N_c=100, N_k=4, W=5, **get_pam_configs())
+
+# Learn two sequence
+pam.learn_sequence(seq1)
+pam.learn_sequence(seq2)
+
+# generate the first sequence
+recall = pam.recall_sequence_offline(seq1[0], len(seq1) - 1)
+
+# Calculate accuracy (IoU)
+print(accuracy_SDR(seq1, recall))
+```
+
+<p align="center">
+  <img src="assets/gen.png" alt="Learning and generating a sequence"/>
+</p>
 
 --- 
 
-## Usage
+## Paper Results Reproducibility
 
 
-```python
-from src.utils.sdr import SDR
-from src.models.pam import PamModel
-from src.utils.configs import get_pam_configs
 
-seq = [SDR(N=100, S=5) for _ in range(10)]
-print(seq)
-pam = PamModel(N_c=100, N_k=4, W=5, **get_pam_configs())
 
-pam.learn_sequence(seq)
-recall = pam.recall_sequence_offline(seq[0], len(seq)-1)
-print(recall)
-```
 
--- src/models/pam.py
+++ src/models/pam.py
@@ -1,23 +1,21 @@
 import torch
 import torch.nn as nn
-import numpy as np
-import random
-from random import shuffle
 
 from src.utils.sdr import SDR
 
 
 class Connections:
-    def __init__(self, 
-                 N_c, 
-                 N_k,
-                 synaptic_density=0.5, 
-                 eta_inc = 0.1,
-                 eta_dec = 0.1,
-                 eta_decay=0.0, 
-                 init_mean=0.0,
-                 init_std=0.1
-                 ):
+    def __init__(
+        self,
+        N_c,
+        N_k,
+        synaptic_density=0.5,
+        eta_inc=0.1,
+        eta_dec=0.1,
+        eta_decay=0.0,
+        init_mean=0.0,
+        init_std=0.1,
+    ):
 
         self.N_c = N_c
         self.N_k = N_k
@@ -36,7 +34,9 @@ class Connections:
     def generate_binary_tensor(self):
         total_elements = torch.prod(torch.tensor(self.W.shape))
         tensor = torch.zeros_like(self.W, dtype=torch.bool)
-        indices = torch.randperm(total_elements)[:int(self.synaptic_density*total_elements)]
+        indices = torch.randperm(total_elements)[
+            : int(self.synaptic_density * total_elements)
+        ]
         row_indices = indices // self.W.shape[1]
         col_indices = indices % self.W.shape[1]
         tensor[row_indices, col_indices] = 1
@@ -44,27 +44,26 @@ class Connections:
 
     def initialize(self):
 
-        self.W = torch.zeros((self.N_c*self.N_k, self.N_c*self.N_k))
+        self.W = torch.zeros((self.N_c * self.N_k, self.N_c * self.N_k))
         nn.init.normal_(self.W, mean=self.init_mean, std=self.init_std)
 
         self.W_mask = self.generate_binary_tensor()
         self.W *= self.W_mask
 
         self.clamp()
-        self.parameters = ['W', 'W_mask']
-
+        self.parameters = ["W", "W_mask"]
 
     def __call__(self, x_in_sdr):
         assert isinstance(x_in_sdr, SDR), "input must be an SDR"
-        return self.W.t()@x_in_sdr.to_nodes()
+        return self.W.t() @ x_in_sdr.to_nodes()
 
     def train_A(self, x_in_sdr, x_out_sdr):
         assert isinstance(x_in_sdr, SDR), "input must be an SDR"
         assert isinstance(x_out_sdr, SDR), "output must be an SDR"
 
         x_in_nodes = x_in_sdr.to_nodes()
-        inc = x_in_nodes@x_out_sdr.to_nodes(reverse=False).t()
-        dec = x_in_nodes@x_out_sdr.to_nodes(reverse=True).t()
+        inc = x_in_nodes @ x_out_sdr.to_nodes(reverse=False).t()
+        dec = x_in_nodes @ x_out_sdr.to_nodes(reverse=True).t()
 
         self.W += (self.eta_inc * inc) - (self.eta_dec * dec) - (self.eta_decay)
         self.W *= self.W_mask
@@ -75,15 +74,14 @@ class Connections:
         assert isinstance(single_sdr, SDR), "single must be an SDR"
 
         single_nodes = single_sdr.to_nodes()
-        inc = single_nodes@single_nodes.t()
-        dec1 = single_nodes@(union_sdr-single_sdr).to_nodes().t()
-        dec2 = (union_sdr-single_sdr).to_nodes().t()@single_nodes
-        dec = dec1+dec2
+        inc = single_nodes @ single_nodes.t()
+        dec1 = single_nodes @ (union_sdr - single_sdr).to_nodes().t()
+        dec2 = (union_sdr - single_sdr).to_nodes().t() @ single_nodes
+        dec = dec1 + dec2
 
         self.W += (self.eta_inc * inc) - (self.eta_dec * dec) - (self.eta_decay)
         self.W *= self.W_mask
         self.clamp()
-
 
     def save(self, path=None):
         to_save = {}
@@ -91,8 +89,10 @@ class Connections:
             attr = getattr(self, p)
             to_save[p] = attr if isinstance(attr, torch.Tensor) else attr.save()
 
-        if path != None: torch.save(to_save, path)
-        else: return to_save
+        if path != None:
+            torch.save(to_save, path)
+        else:
+            return to_save
 
     def load(self, path):
         parameters = torch.load(path) if isinstance(path, str) else path
@@ -107,13 +107,14 @@ class Connections:
 
 class PamModel:
 
-    def __init__(self,
-                 N_c, 
-                 N_k,
-                 W,
-                 transition_configs,
-                 emission_configs,
-                 ):
+    def __init__(
+        self,
+        N_c,
+        N_k,
+        W,
+        transition_configs,
+        emission_configs,
+    ):
 
         self.N_c = N_c
         self.N_k = N_k
@@ -121,52 +122,56 @@ class PamModel:
         self.transition_configs = transition_configs
         self.emission_configs = emission_configs
 
-        self.configs = ['N_c', 'N_k', 'W', 'transition_configs', 'emission_configs']
+        self.configs = ["N_c", "N_k", "W", "transition_configs", "emission_configs"]
         self.initialize()
 
     def initialize(self):
 
         self.transition = Connections(
-                            N_c = self.N_c, 
-                            N_k = self.N_k,   
-                            synaptic_density = self.transition_configs['synaptic_density'],
-                            eta_inc = self.transition_configs['eta_inc'], 
-                            eta_dec = self.transition_configs['eta_dec'], 
-                            eta_decay = self.transition_configs['eta_decay'],
-                            init_mean = self.transition_configs['init_mean'],
-                            init_std = self.transition_configs['init_std']
-                            )
+            N_c=self.N_c,
+            N_k=self.N_k,
+            synaptic_density=self.transition_configs["synaptic_density"],
+            eta_inc=self.transition_configs["eta_inc"],
+            eta_dec=self.transition_configs["eta_dec"],
+            eta_decay=self.transition_configs["eta_decay"],
+            init_mean=self.transition_configs["init_mean"],
+            init_std=self.transition_configs["init_std"],
+        )
 
         self.emission = Connections(
-                            N_c = self.N_c, 
-                            N_k = 1,
-                            synaptic_density = self.emission_configs['synaptic_density'],
-                            eta_inc = self.emission_configs['eta_inc'], 
-                            eta_dec = self.emission_configs['eta_dec'], 
-                            eta_decay = self.emission_configs['eta_decay'],
-                            init_mean = self.emission_configs['init_mean'],
-                            init_std = self.emission_configs['init_std']
-                            )
+            N_c=self.N_c,
+            N_k=1,
+            synaptic_density=self.emission_configs["synaptic_density"],
+            eta_inc=self.emission_configs["eta_inc"],
+            eta_dec=self.emission_configs["eta_dec"],
+            eta_decay=self.emission_configs["eta_decay"],
+            init_mean=self.emission_configs["init_mean"],
+            init_std=self.emission_configs["init_std"],
+        )
 
-        self.start_sdr = self.create_max_sdr(logits=torch.randn((self.N_c*self.N_k, 1)))
+        self.start_sdr = self.create_max_sdr(
+            logits=torch.randn((self.N_c * self.N_k, 1))
+        )
         self.duty_cycle = torch.zeros((self.N_c, self.N_k))
-        self.parameters = ['transition', 'emission', 'start_sdr']
+        self.parameters = ["transition", "emission", "start_sdr"]
 
     def create_max_sdr(self, logits):
         logits_reshaped = logits.reshape(self.N_c, self.N_k)
         max_indices = torch.argmax(logits_reshaped, dim=1)
         scatter_indices = torch.arange(0, self.N_c) * self.N_k + max_indices
-        return SDR(self.N_c*self.N_k, ix=scatter_indices)
+        return SDR(self.N_c * self.N_k, ix=scatter_indices)
 
     def boost(self, a, bs):
-        boost_factor = self.duty_cycle/(self.duty_cycle.sum(-1, keepdim=True)+1e-8)
-        a += bs*((1/self.N_k)-boost_factor).reshape(a.shape) + torch.normal(mean=0.0, std=0.1, size=a.shape)
+        boost_factor = self.duty_cycle / (self.duty_cycle.sum(-1, keepdim=True) + 1e-8)
+        a += bs * ((1 / self.N_k) - boost_factor).reshape(a.shape) + torch.normal(
+            mean=0.0, std=0.1, size=a.shape
+        )
         return a
 
     def threshold_a(self, a):
         a = a.reshape(self.N_c, self.N_k)
         a_best = torch.zeros_like(a)
-        a_best[a >= self.W*self.transition_configs['threshold']] = 1.0
+        a_best[a >= self.W * self.transition_configs["threshold"]] = 1.0
         a_best += torch.normal(mean=0.0, std=0.01, size=a_best.shape)
         return a_best
 
@@ -174,7 +179,9 @@ class PamModel:
         return SDR.from_bin(torch.logical_and(feature.bin, self.start_sdr.bin))
 
     def forward_transition(self, z):
-        return SDR.from_nodes_threshold( self.transition(z), threshold=self.transition_configs['threshold']*len(z))
+        return SDR.from_nodes_threshold(
+            self.transition(z), threshold=self.transition_configs["threshold"] * len(z)
+        )
 
     def evaluate_transition(self, z, p):
         return p in self.forward_transition(z).reduce(self.N_k)
@@ -183,10 +190,14 @@ class PamModel:
         z_pred_single_input = z_pred_single
         for i in range(iters):
             z_pred_out = self.emission(z_pred_single_input)
-            z_pred_single_output = SDR.from_nodes_threshold(z_pred_out, threshold=self.emission_configs['threshold']*len(z_pred_single_input))
+            z_pred_single_output = SDR.from_nodes_threshold(
+                z_pred_out,
+                threshold=self.emission_configs["threshold"] * len(z_pred_single_input),
+            )
             z_pred_single_output = z_pred_single_output.intersect(z_pred)
 
-            if z_pred_single_input == z_pred_single_output: break
+            if z_pred_single_input == z_pred_single_output:
+                break
             z_pred_single_input = z_pred_single_output
 
         return z_pred_single_output
@@ -204,29 +215,38 @@ class PamModel:
 
             # train transition
             a = self.transition(z)
-            z_new = p.expand(self.N_k).intersect(self.create_max_sdr(self.threshold_a(a)))
+            z_new = p.expand(self.N_k).intersect(
+                self.create_max_sdr(self.threshold_a(a))
+            )
 
             for i in range(1000):
                 self.transition.train_A(z, z_new)
-                if self.evaluate_transition(z, p): break
+                if self.evaluate_transition(z, p):
+                    break
 
             # train emission
-            z_pred = SDR.from_nodes_threshold(self.transition(z), threshold=self.transition_configs['threshold']*len(z)).reduce(self.N_k)
+            z_pred = SDR.from_nodes_threshold(
+                self.transition(z),
+                threshold=self.transition_configs["threshold"] * len(z),
+            ).reduce(self.N_k)
             for i in range(1000):
                 self.emission.train_B(z_pred, p)
-                if self.evaluate_emission(z_pred, p): break
+                if self.evaluate_emission(z_pred, p):
+                    break
 
             # update z
             z = z_new
 
     def recall_sequence_online(self, sequence):
-        """ only used for debugging"""
+        """only used for debugging"""
         recall = [sequence[0]]
         z = self.encode_start(sequence[0].expand(self.N_k))
         for p in sequence[1:]:
             a = self.transition(z)
 
-            z_pred = SDR.from_nodes_threshold(a, threshold=self.transition_configs['threshold']*len(z))
+            z_pred = SDR.from_nodes_threshold(
+                a, threshold=self.transition_configs["threshold"] * len(z)
+            )
             recall.append(z_pred.reduce(self.N_k))
 
             z = p.expand(self.N_k).intersect(self.create_max_sdr(a))
@@ -240,11 +260,14 @@ class PamModel:
 
             # transition
             a = self.transition(z)
-            z_pred = SDR.from_nodes_threshold(a, threshold=self.transition_configs['threshold']*len(z)).reduce(self.N_k)
+            z_pred = SDR.from_nodes_threshold(
+                a, threshold=self.transition_configs["threshold"] * len(z)
+            ).reduce(self.N_k)
 
             # attractors
             z_pred_single = self.forward_emission(z_pred, p)
-            if len(z_pred_single) == 0: z_pred_single = p.choose(self.W)
+            if len(z_pred_single) == 0:
+                z_pred_single = p.choose(self.W)
             generated.append(z_pred_single)
 
             # update states
@@ -252,9 +275,8 @@ class PamModel:
 
         return generated
 
-
     def recall_sequence_offline(self, x_in_sdr, seq_len):
-        """ only used for debugging"""
+        """only used for debugging"""
         recall = [x_in_sdr]
         z = self.encode_start(x_in_sdr.expand(self.N_k))
         for _ in range(seq_len):
@@ -264,7 +286,6 @@ class PamModel:
 
         return recall
 
-
     def generate_sequence_offline(self, x_in_sdr, seq_len):
         generated = [x_in_sdr]
 
@@ -273,15 +294,20 @@ class PamModel:
 
             # transition
             a = self.transition(z)
-            z_pred = SDR.from_nodes_threshold(a, threshold=self.transition_configs['threshold']*len(z)).reduce(self.N_k)
+            z_pred = SDR.from_nodes_threshold(
+                a, threshold=self.transition_configs["threshold"] * len(z)
+            ).reduce(self.N_k)
 
             # attractors
             for i in range(100):
                 z_pred_single = self.forward_emission(z_pred, z_pred.choose(1))
-                if len(z_pred_single) == self.W: break
+                if len(z_pred_single) == self.W:
+                    break
 
-            if len(z_pred_single) == 0: z_pred_single = z_pred.choose(self.W)
-            if len(z_pred_single) > self.W: z_pred_single = z_pred_single.choose(self.W)
+            if len(z_pred_single) == 0:
+                z_pred_single = z_pred.choose(self.W)
+            if len(z_pred_single) > self.W:
+                z_pred_single = z_pred_single.choose(self.W)
 
             generated.append(z_pred_single)
 
@@ -290,24 +316,26 @@ class PamModel:
 
         return generated
 
-
     def save(self, path=None):
         to_save = {}
         for p in self.parameters:
             attr = getattr(self, p)
-            to_save[p] = attr if isinstance(attr, torch.Tensor) else attr.save(path=None)
+            to_save[p] = (
+                attr if isinstance(attr, torch.Tensor) else attr.save(path=None)
+            )
 
-        if path != None: 
-            to_save['configs'] = {c:getattr(self,c) for c in self.configs}
+        if path != None:
+            to_save["configs"] = {c: getattr(self, c) for c in self.configs}
             torch.save(to_save, path)
-        else: return to_save
-
+        else:
+            return to_save
 
     def load(self, path):
         parameters = torch.load(path) if isinstance(path, str) else path
 
         for name, weight in parameters.items():
-            if name not in self.parameters: continue
+            if name not in self.parameters:
+                continue
 
             attr = getattr(self, name)
             if isinstance(attr, torch.Tensor):
@@ -315,11 +343,9 @@ class PamModel:
             else:
                 attr.load(weight)
 
-
     @staticmethod
     def load_model(path):
         loaded = torch.load(path)
-        model = PamModel(**loaded['configs'])
+        model = PamModel(**loaded["configs"])
         model.load(loaded)
         return model
-
